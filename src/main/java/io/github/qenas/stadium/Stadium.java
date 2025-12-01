219ab4074e6a4384cce834247025703ff86620d3
Claudio Vasquez <clauvzq150@gmail.com>
2025-12-01 00:34:26 -0300

    Initial commit.

---
+++ src/main/java/io/github/qenas/stadium/Stadium.java
@@ -0,0 +1,17 @@
+package io.github.qenas.stadium;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Stadium extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
